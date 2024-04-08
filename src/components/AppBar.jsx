5ae813add31c55888231b1e5d9cf1c3d62c47a34
Gijs Verdonk <g.verdonk@icloud.com>
2024-04-08 16:56:49 +0200

    Small href fix.

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -50,7 +50,7 @@ function ResponsiveAppBar() {
                         variant="h6"
                         noWrap
                         component="a"
-                        href="#app-bar-with-responsive-menu"
+                        href=""
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
@@ -104,7 +104,7 @@ function ResponsiveAppBar() {
                         variant="h5"
                         noWrap
                         component="a"
-                        href="#app-bar-with-responsive-menu"
+                        href=""
                         sx={{
                             mr: 2,
                             display: { xs: 'flex', md: 'none' },
