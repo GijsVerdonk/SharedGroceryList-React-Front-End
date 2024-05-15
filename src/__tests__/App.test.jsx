66225a69d62cf14816a022a76d289bb00e0f6740
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-15 20:21:03 +0200

    Write first test.

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '/src/App.jsx';
+/* eslint-disable */
+
+
+test('test that by default a user is logged out', () => {
+    render(<App title="React" />);
+    const alert = screen.getByText(/Log in om toegang te krijgen tot alle functionaliteit./i);
+    expect(alert).toBeInTheDocument();
+});
