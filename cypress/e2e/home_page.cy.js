02c84c91d7991d9580d997f0f960bb11fa9ee349
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-20 15:15:00 +0200

    Changed E2E tests.

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -10,30 +10,28 @@ describe('Login flow', () => {
         cy.contains('SGL');
     });
 
-    it('successfully login to an account that DOES NOT exists', () => {
-        cy.visit('/');
-        cy.get('.center-button > .MuiButtonBase-root').click();
-
-        cy.origin('https://dev-1qptdla0pgqbqxfn.us.auth0.com', () => {
-            cy.contains('Log in to dev-1qptdla0pgqbqxfn to continue to SharedGroceryList.');
-            cy.get('input[name="username"]').type("aaaa@account.com");
-            cy.get('input[name="password"]').type("wrongpassword");
-            cy.get('button[name="action"]').click();
-
-            cy.contains('Wrong email or password');
-        });
-
-    });
+    // it('successfully login to an account that DOES NOT exists', () => {
+    //     cy.visit('/');
+    //     cy.get('.center-button > .MuiButtonBase-root').click();
+    //
+    //     cy.origin('https://dev-1qptdla0pgqbqxfn.us.auth0.com', () => {
+    //         cy.contains('Log in to dev-1qptdla0pgqbqxfn to continue to SharedGroceryList.');
+    //         cy.get('input[name="username"]').type("aaaa@account.com");
+    //         cy.get('input[name="password"]').type("wrongpassword");
+    //         cy.get('button[name="action"]').click();
+    //
+    //         cy.contains('Wrong email or password');
+    //     });
+    //
+    // });
 });
 
 describe('after login', () => {
         it('successfully login to an account that DOES exists', () => {
             cy.login('appel@gmail.com', 'CF5UnRy2uk*tdAEfMt4*Vv2DgUY');
             cy.contains('Rows per page:');
-            //TODO: add more tests.
-            cy.get('#outlined-basic').type("List Z");
-            cy.get('form > .MuiButtonBase-root').click();
-            cy.get('#outlined-basic').type("");
-            cy.contains('List Z');
+            cy.wait(10000);
+            cy.get(':nth-child(2) > .MuiButton-root').click();
+            cy.contains('List A');
         });
     });
