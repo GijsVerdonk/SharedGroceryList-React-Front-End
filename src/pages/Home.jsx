2ba35f88e0eb6b607b6e1671cc08bc8140f94d46
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-22 11:14:59 +0200

    Fetch items from AI model.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Profile from "../components/Profile.jsx";
 import CreateGroceryListForm from "../components/CreateGroceryListForm.jsx";
 import UserListDataGrid from "../components/UserListDataGrid.jsx"
+import RecipeFromAI from "../components/RecipeFromAI.jsx"
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
         <Profile></Profile>
         <UserListDataGrid></UserListDataGrid>
         <CreateGroceryListForm></CreateGroceryListForm>
+        <RecipeFromAI></RecipeFromAI>
     </div>
   );
 }
