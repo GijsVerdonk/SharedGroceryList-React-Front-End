24216839eda904bb6cab63f6d7c2648cc29e3a3c
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-22 17:09:00 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: 'oixxyt',
   e2e: {
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://localhost:5173',
+    // baseUrl: 'http://localhost:4173',
     setupNodeEvents(on, config) {
     },
   },
