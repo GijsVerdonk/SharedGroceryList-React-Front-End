3eaa1a27cf8cd129719c728efe8659e84d2740a4
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-22 15:11:56 +0200

    Create cypress.prod.js

---
+++ cypress.prod.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'oixxyt',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
