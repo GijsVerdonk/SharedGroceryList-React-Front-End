6ac85c9c3bc44f6cef648cbce6842ee2405733e9
Gijs Verdonk <g.verdonk@icloud.com>
2024-05-29 11:20:11 +0200

    Add loading to List component.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,32 +1,51 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import ListItemDataGrid from "./ListItemDataGrid.jsx";
-import AddListItemForm from "./AddListItemForm.jsx";
 import Typography from "@mui/material/Typography";
 import DeleteListButton from "./DeleteListButton.jsx";
-import RecipeFromAI from "./RecipeFromAI.jsx";
 import * as React from "react";
+import Cookies from "js-cookie";
+import {Alert} from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const List = () => {
   const { id } = useParams();
   const [list, setList ] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+
+    const accessToken = Cookies.get('accessToken');
     const getList = async () => {
-      const response = await fetch(`https://localhost:7001/api/List/${id}`);
+      const response = await fetch(`https://localhost:7001/api/List/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+      setLoading(true);
       if (response.ok) {
         const data = await response.json();
         setList(data);
         console.log(data);
+        setLoading(false);
       } else {
         console.log("Error.")
+        setLoading(false);
       }
     };
 
     getList();
   }, []);
 
-  if (list) {
+  if (loading) {
+    return (
+        <div>
+          <CircularProgress sx={{ m: 2 }}/>
+        </div>
+    )
+  }
+
+  if (list && !loading) {
     return (
         <div>
 
@@ -41,8 +60,9 @@ const List = () => {
 
     return (
         <div>
-        <h1>Lijst niet gevonden.</h1>
-            {/*<RecipeFromAI></RecipeFromAI>*/}
+          <Alert variant="filled" severity="error">
+            Lijst is niet gevonden.
+          </Alert>
       </div>
   );
 };
